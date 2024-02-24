29b3ce25011c28aef72af149e0fa28f7eb0f2465
vad-orl <vad-orl90@mail.ru>
2024-02-25 02:16:45 +0300

    исправлен any в typeguards

--- src/utils/typeguards.ts
+++ src/utils/typeguards.ts
@@ -1,3 +1,3 @@
-import { GithubUser } from 'types';
+import { GithubError, GithubUser } from 'types';
 
-export const isGithubUser = (user: any): user is GithubUser => 'id' in user;
+export const isGithubUser = (user: GithubError | GithubUser): user is GithubUser => 'id' in user;
